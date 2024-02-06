09fafbaad119d83754bad7d4915bf102f26f857e
MateusFindev <finprogramador@gmail.com>
2024-02-06 13:29:42 -0300

    FrontEnd update

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="w-full min-h-screen bg-gray-900 flex justify-center px-4">
+      <main className="my-10 w-full md:max-w-2xl">
+        <h1 className="text-4xl font-medium text-white">Clientes</h1>
+      </main>
+    </div>
+  )
+}
